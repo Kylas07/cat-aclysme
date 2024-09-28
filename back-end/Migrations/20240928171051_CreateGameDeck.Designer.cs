5fa0b33eed82dfddf7cd4974e71eb616e09f2b23
Ghillian <ghil.schindler@gmail.com>
2024-09-28 19:32:32 +0200

    migration?

--- back-end/Migrations/20240928171051_CreateGameDeck.Designer.cs
+++ back-end/Migrations/20240928171051_CreateGameDeck.Designer.cs
@@ -3,6 +3,7 @@ using CatAclysmeApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace back_end.Migrations
 {
     [DbContext(typeof(CatAclysmeContext))]
-    partial class CatAclysmeContextModelSnapshot : ModelSnapshot
+    [Migration("20240928171051_CreateGameDeck")]
+    partial class CreateGameDeck
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
